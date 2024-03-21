6fc9454389f049da5fb4aa895791d407226e2e6a
Niv Sardi <xaiki@evilgiggle.com>
2024-03-21 11:46:37 -0300

    scatfold
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,16 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "./",
+  test: {
+    globals: true,
+  },
+});
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
